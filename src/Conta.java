06c6becdc80d717a1f48b27f1c0b2030b26438bf
Guilherme <guimsjesus@gmail.com>
2024-07-12 16:10:57 -0300

    feat: Implementando métodos sacar depositar e transferir

--- src/Conta.java
+++ src/Conta.java
@@ -1,22 +1,37 @@
 public abstract class Conta implements IConta{
 
-    private int agencia;
-    private int numero;
-    private double saldo;
+    private static final int  AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+    public Conta() {
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
+
+    protected void imprimirInfosComuns() {
+        System.out.println(String.format("Agencia: %d", this.agencia));
+        System.out.println(String.format("Numero: %d", this.numero));
+        System.out.println(String.format("Saldo: %.2f", this.saldo));
+    }
 
     @Override
     public void sacar(double valor) {
-
+        saldo -= valor;
     }
 
     @Override
     public void depositar(double valor) {
-
+        saldo += valor;
     }
 
     @Override
     public void transferir(double valor, Conta contaDestino) {
-
+        this.sacar(valor);
+        contaDestino.depositar(valor);
     }
 
     public int getAgencia() {
